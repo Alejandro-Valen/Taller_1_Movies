9f7148f59a3a250a58fb03d07b1a96b8447eb2fe
Alejandro-Valen <jhonvalencia1112966@correo.itm.edu.co>
2025-09-06 16:38:55 -0500

    cambio de la funcion about

--- movie/views.py
+++ movie/views.py
@@ -8,4 +8,5 @@ def home(request):
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Jhon Alejandro Valencia Gonzalez'})
 def about(request):
-    return HttpResponse ('<h1>Welcome to about page</h1>')
+    #return HttpResponse ('<h1>Welcome to about page</h1>')
+    return render(request, 'about.html', {'name':'Jhon Alejandro Valencia Gonzalez'})
